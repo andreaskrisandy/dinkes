40ef47602160f2928f01e5006457e52ab043b6d3
Adriel Aritonang <adrielaritonang@gmail.com>
2017-07-26 10:53:03 +0700

    module yankesmob

--- yankesmob.php
+++ yankesmob.php
@@ -1,1 +1,14 @@
-<h2> Ini Yankesmob </h2>
+<?php
+	$yankesmob_query = mysql_query("select * from yankesmob");
+	$yankesmob_data = mysql_fetch_array($yankesmob_query);
+	?>
+	<h2>Pelayanan Kesehatan Mobilitas</h2>
+	<div style="padding-bottom:50px;">
+		<center>
+			<img style="max-width:100%;max-height:100%;" src="yankesmob/<?=$yankesmob_data[gambar]?>"/>
+		</center>
+	</div>
+  <p>
+    <?= $yankesmob_data[deskripsi]?>
+  </p>
+<?php
